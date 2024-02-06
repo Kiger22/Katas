ccd49f94b1c1d9004344999c7787c6b549caebde
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-02-06 12:27:35 +0100

    Create ejercicio16.js

---
+++ Ejercicio JS/ejercicio16.js
@@ -0,0 +1,16 @@
+/* Probando For...of: Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores.
+
+Puedes usar este array: */
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const places of placesToTravel) {
+  console.log(places)
+
+}
+
+for (let i = 0; i < placesToTravel.length; i++) {
+  const element = placesToTravel[i];
+  console.log(element)
+
+}
