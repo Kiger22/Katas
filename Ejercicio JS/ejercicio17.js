a08522a44e38fceedfbe437f3c867635cc74fa06
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-02-06 12:47:58 +0100

    Update ejercicio17.js

---
+++ Ejercicio JS/ejercicio17.js
@@ -0,0 +1,16 @@
+/* Probando For...in: 
+Usa un for...in para imprimir por consola los datos del alienígena..
+ Puedes usar este objeto: */
+
+const alien = {
+  name: 'Wormuck',
+  race: 'Cucusumusu',
+  planet: 'Eden',
+  weight: '259kg'
+}
+
+console.log('Datos del Alien: ')
+for (const key in alien) {
+  console.log(key + ' : ' + alien[key])
+
+}
